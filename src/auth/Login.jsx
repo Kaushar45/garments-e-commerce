b321cce4515c54078556c1d69431a3265d77f2d3
Kaushar45 <45kaushar@gmail.com>
2025-06-30 18:19:46 +0530

    login & signup page

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login Page</div>;
+  return (
+    <form
+      style={{
+        display: "grid",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "5px",
+      }}
+    >
+      <h1>User Login</h1>
+      <input required placeholder="Username" name="Username" type="email" />
+      <input required placeholder="Password" name="password" type="password" />
+    </form>
+  );
 };
 
 export default Login;
--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Signup = () => {
-  return <div>Signup Page</div>;
+  return (
+    <form
+      style={{
+        display: "grid",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "5px",
+      }}
+    >
+      <h1>User Signup</h1>
+      <input required placeholder="Username" name="Username" type="email" />
+      <input required placeholder="Email" name="email" type="email" />
+      <input required placeholder="Full Name" name="fullName" type="text" />
+      <input
+        required
+        placeholder="Phone Number"
+        name="phoneNumber"
+        type="tel"
+      />
+      <input required placeholder="Password" name="password" type="password" />
+    </form>
+  );
 };
 
 export default Signup;
